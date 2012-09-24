c4138720ab1a20e0c2f84997daac92b6e1db7341
Norbert Toth-Gati <norbert.tothgati@gmail.com>
2012-09-24 23:28:25 +0200

    Implemented initial layout using Gridview

--- source/src/org/tony/play/memogame/GameActivity.java
+++ source/src/org/tony/play/memogame/GameActivity.java
@@ -1,8 +1,13 @@
 package org.tony.play.memogame;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class GameActivity extends Activity {
 
@@ -10,6 +15,15 @@ public class GameActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        
+        GridView gridview = (GridView) findViewById(R.id.gameGridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(GameActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });        
     }
 
     @Override
